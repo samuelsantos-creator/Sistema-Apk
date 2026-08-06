2eaccd2c92ee66c8b538dd9c19dbf1de165eea74
samuelsantos-creator <samuel.santos@progeral.com.br>
2026-08-06 17:19:17 -0300

    fix: adiciona suporte a requisicoes CORS Preflight (OPTIONS) nos scripts PHP proxy e get_ops

--- api/get_ops.php
+++ api/get_ops.php
@@ -7,7 +7,13 @@
 
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Cache-Control');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Configurações de Conexão (Fornecidas pelo usuário)
 $host = '192.168.8.12';
